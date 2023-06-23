97a4aefd91bea9be64dfa3df0559ce6d858db2d7
aoeII <79325762+aoeII@users.noreply.github.com>
2023-06-23 20:13:08 +0800

    Update diy-part2_me.sh

--- diy-part2_me.sh
+++ diy-part2_me.sh
@@ -36,3 +36,4 @@ rm -rf .package/sirpdboy-package/luci-app-autotimeset
 # svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
 # rm -rf feeds/packages/lang/golang
 # svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
+rm -rf package/lean/autocore
